33d79c033f47f5780c84158a928493a29b250a7c
DonLarry <donlarry.timoteo@gmail.com>
2022-07-16 22:21:13 -0400

    Added Carlo's documentation

--- chess_engine/minimax.py
+++ chess_engine/minimax.py
@@ -1,3 +1,4 @@
+# Se asignan valores para cada pieza, ya sea positivo o negativo según el color
 piece_value = {
   None: 0,
   'p': -10,
@@ -16,20 +17,27 @@ piece_value = {
 
 
 def evaluation(board):
+  """ Función de evaluación del tablero """
   eval = 0
+  # Para todas las piezas presentes en el tablero se suman sus valores, dependiendo de la pieza y el color
+  # esto produce un número que representa la evaluación actual del tablero usada en el minimax.
   for piece in board.piece_map().values():
     eval += piece_value[str(piece)]
   return eval
 
 
 def minimax(board, depth=3, alpha=-10000, beta=10000, white=True):
+  """ Implementación recursiva del algoritmo minimax """
+  # Se obtienen los movimientos posibles para el tablero actual
   legal_moves = [move for move in board.legal_moves]
 
+  # Al llegar a la profundidad deseada, se aplica la función de evaluación
   if depth==0 or not legal_moves:
     return evaluation(board), []
 
   best_moves = []
 
+  # Se elige el criterio de comparación según el color de las piezas
   if white:
     compare = lambda a, b: a > b
     best_eval = -9999
@@ -37,17 +45,23 @@ def minimax(board, depth=3, alpha=-10000, beta=10000, white=True):
     compare = lambda a, b: a < b
     best_eval = 9999
 
+  # Se corre el algoritmo con todos los movimientos posibles
+
   for move in board.legal_moves:
+    # Se simula un movimiento en el tablero para calcular que beneficio aporta
     board.push(move)
     current_eval, _ = minimax(board, depth-1, alpha, beta, not white)
+    # Se regresa el tablero al estado anterior, puesto que solo era una simulación
     board.pop()
 
+    # Se compara el beneficio del movimiento con el mejor hallado hasta el momento, con la esperanza de actualizar este último
     if compare(current_eval, best_eval):
       best_eval = current_eval
       best_moves = [move]
     elif current_eval == best_eval:
       best_moves.append(move)
 
+    # Aplicación de la poda alfa beta
     alpha = max(alpha, current_eval)
     if beta <= alpha:
       break
--- chess_engine/utils.py
+++ chess_engine/utils.py
@@ -4,21 +4,24 @@ import random
 import IPython
 from .minimax import minimax
 
-
+# Funcion de utilidad para mostrar un tablero dibujado como un SVG
 def display_board(board, *args, size=390, **kwargs):
   svg = chess.svg.board(board, *args, size=size, **kwargs)
   display(IPython.display.HTML(svg))
 
 
+# Loop de juego principal
 def play():
   board = chess.Board()
   turn = 0
   display_board(board)
   while True:
+    # Se verifica si la partida ha terminado y en caso afirmativo muestra la causa
     outcome = board.outcome()
     if outcome:
       print(f'Game over: {outcome.result()} ({outcome.termination.name})')
       break
+    # En los turnos pares, juega la persona, en los impares, juega el agente
     if not turn & 1:
       instruction = input("Enter move: ")
       if instruction in {'quit', 'resign'}:
