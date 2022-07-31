c935f662512ff71947a87d6ba302ea893c4d564b
DonLarry <donlarry.timoteo@gmail.com>
2022-07-30 23:56:37 -0400

    Modified minimax: returns all the possible moves with their evaluation

--- chess_engine/minimax.py
+++ chess_engine/minimax.py
@@ -1,3 +1,8 @@
+from typing import Tuple, List
+
+from chess import Move
+
+
 # Values are assigned for each piece, either positive or negative depending on the color.
 piece_value = {
   None: 0,
@@ -30,17 +35,16 @@ def evaluation(board):
   return eval
 
 
-def minimax(board, depth=3, alpha=-10000, beta=10000, white=True):
-  """Recursive implementation of the minimax algorithm."""
+def _minimax(board, depth=2, alpha=-10000, beta=10000, white=True) -> int:
+  """This is a pure minimax recursive algorithm that gives an evaluation for
+  a node in the minimax tree."""
 
   # The possible moves for the current board are obtained.
   legal_moves = [move for move in board.legal_moves]
 
   # When the target depth is reached, the evaluation function is applied.
   if depth==0 or not legal_moves:
-    return evaluation(board), []
-
-  best_moves = []
+    return evaluation(board)
 
   # The comparison criterion is chosen according to the color of the pieces.
   if white:
@@ -55,7 +59,7 @@ def minimax(board, depth=3, alpha=-10000, beta=10000, white=True):
   for move in board.legal_moves:
     # A move is simulated on the board to calculate what benefit it brings.
     board.push(move)
-    current_eval, _ = minimax(board, depth-1, alpha, beta, not white)
+    current_eval = _minimax(board, depth-1, alpha, beta, not white)
 
     # The board is returned to the previous state, since it was only a simulation.
     board.pop()
@@ -63,13 +67,44 @@ def minimax(board, depth=3, alpha=-10000, beta=10000, white=True):
     # The benefit of the movement is compared with the best one found so far, in the hope of updating the latter.
     if compare(current_eval, best_eval):
       best_eval = current_eval
-      best_moves = [move]
-    elif current_eval == best_eval:
-      best_moves.append(move)
 
     # Alpha-beta pruning.
     alpha = max(alpha, current_eval)
     if beta <= alpha:
       break
 
-  return best_eval, best_moves
+  # The evaluation for the current node is the best one found.
+  return best_eval
+
+
+def minimax(board, depth=3, alpha=-10000, beta=10000, white=True) -> List[Tuple[int, Move]]:
+  """Custom minimax algorithm. Returns a list of all the possible moves represented
+  as pairs (evaluation, move)."""
+
+  # The possible moves for the current board are obtained.
+  legal_moves = [move for move in board.legal_moves]
+
+  # If there's no legal moves, either the position is checkmate, or stalemate
+  if not legal_moves:
+    return [(evaluation(board), None)]
+
+  evaluations = []
+
+  # The algorithm is run with all the possible movements.
+
+  for move in board.legal_moves:
+    # A move is simulated on the board to calculate what benefit it brings.
+    board.push(move)
+    current_eval = _minimax(board, depth-1, alpha, beta, not white)
+    evaluations.append(current_eval)
+
+    # The board is returned to the previous state, since it was only a simulation.
+    board.pop()
+
+    # Alpha-beta pruning.
+    alpha = max(alpha, current_eval)
+    if beta <= alpha:
+      break
+
+  evaluations_and_moves = zip(evaluations, legal_moves)
+  return sorted(evaluations_and_moves, reverse=True, key=lambda t: t[0])
